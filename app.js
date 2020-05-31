cc43487834ef7f70af6735e17e140a7ff06cd3f9
Robert Salmon <robdavidsalmon@msn.com>
2020-05-31 21:05:08 +0100

    Add call to Github, search box, countdown and scoll to user details

--- app.js
+++ app.js
@@ -1,16 +1,58 @@
-var app = angular.module('plunker', []);
+(function() {
+var app = angular.module('githubProfileViewer', []);
 
-app.controller('GetGithubUserDetails', function($scope, $http) {
+var MainController =  function($scope, $http, $interval, $log, $anchorScroll, $location) {
   
 var onUserComplete = function(response) {
   $scope.user = response.data;
+  $http.get($scope.user.repos_url)
+  .then(onRepos, onError);
 }; 
 
 var onError = function(reason) {
-  $scope.error = "Could not fetch my details from GitHub";
+  $scope.error = "Could not fetch the user";
 };
 
-$http.get("https://aps.github.com/users/robsalmon")
-.then(onUserComplete, onError);
+var onRepos = function(response) {
+$scope.repos = response.data;
+$location.hash("userDetails");
+$anchorScroll();
+};
 
-});
+var decrementCountdown = function() {
+$scope.countdown -= 1;
+
+if($scope.countdown < 1) {
+
+  $scope.search($scope.username);
+}
+};
+
+var countdownInterval = null;
+
+var startCountdown = function() {
+ countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+};
+
+$scope.search = function(username) {
+  $log.info("Searching for " + username);
+  $http.get("https://api.github.com/users/" + username)
+  .then(onUserComplete, onError);
+
+  if (countdownInterval) {
+$interval.cancel(countdownInterval);
+$scope.countdown = null;
+  }
+};
+
+
+
+$scope.username = "robsalmon";
+$scope.message = "Github profile viewer";
+$scope.repoSortOrder = "-stargazers_count";
+$scope.countdown = 5;
+startCountdown();
+};
+
+app.controller("MainController", MainController);
+}());
